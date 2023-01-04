41c29f00e5246f0a72b79ca3bf4b6a4721942665
Wade chen <kyzusmugoe@gmail.com>
2023-01-05 00:42:12 +0800

    沒改甚麼

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 import './sass/index.sass'
-import Swiper, { Pagination, Navigation, Controller, Thumbs, Lazy, Autoplay, EffectFade } from 'swiper';
+//import Swiper, { Pagination, Navigation, Controller, Thumbs, Lazy, Autoplay, EffectFade } from 'swiper';
